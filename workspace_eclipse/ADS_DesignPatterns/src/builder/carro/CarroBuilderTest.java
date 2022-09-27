8a3df3b39dae18fabe9b1c29784727f4a365cb66
Mariana <87673848+MaMBassi@users.noreply.github.com>
2022-09-27 09:53:35 -0300

    Add files via upload
    Aula Design Patterns

---
+++ workspace_eclipse/ADS_DesignPatterns/src/builder/carro/CarroBuilderTest.java
@@ -0,0 +1,35 @@
+package builder.carro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CarroBuilderTest {
+
+	@Test
+	void deveConstruirUmCarroPeloBuilder() {
+		//partes
+		Porta portaMotorista = new Porta();
+		Porta portaPassageiro = new Porta();
+		Porta[] portas = {portaMotorista,portaPassageiro};
+		Motor motorVTEC = new Motor();
+		ComputadorBordo computadorSiemens = new ComputadorBordo();
+		FreioABS freioBosh = new FreioABS();
+		
+		//builder
+		CarroBuilder carroBuilder = new CarroBuilder();
+		carroBuilder.buildPortas(portas);
+		carroBuilder.buildMotor(motorVTEC);
+		carroBuilder.buildComputadorBordo(computadorSiemens);
+		carroBuilder.buildFreioABS(freioBosh);
+		
+		//testa o carro construido
+		Carro carroConstruido = carroBuilder.getCarro();
+		assertNotNull(carroConstruido);
+		assertNotNull(carroConstruido.getPortas());
+		assertNotNull(carroConstruido.getMotor());
+		assertNotNull(carroConstruido.getComputadorBordo());
+		assertNotNull(carroConstruido.getFreioABS());
+	}
+
+}
