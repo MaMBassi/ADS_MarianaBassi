32aa34341f79dba23d860967c254b5d5f76baf3c
Mariana <87673848+MaMBassi@users.noreply.github.com>
2022-10-03 21:14:10 -0300

    Add files via upload
    Exercicio Pizzaria

---
+++ workspace_eclipse/ADS_DesignPatterns/src/pizzaria/PizzaBuilder.java
@@ -0,0 +1,47 @@
+package builder.pizzaria;
+
+public class PizzaBuilder {
+	
+	private Pizza pizza;
+	
+	public PizzaBuilder(){
+		this.pizza = new Pizza();
+	}
+	
+	public PizzaBuilder setSize(Integer size) {
+		pizza.setSize(size);
+		return this;
+	}
+	
+	public PizzaBuilder addCheese() {
+		pizza.setFlagCheese(true);
+		return this;
+	}
+	
+	public PizzaBuilder addPepperoni() {
+		pizza.setFlagPepperoni(true);
+		return this;
+	}
+	
+	public Pizza getPizza() {
+		validarIngredientes();
+		validarTamanho();
+		return pizza;
+	}
+	
+	private void validarIngredientes() {
+		if(pizza.getFlagCheese() == false && pizza.getFlagPepperoni() == false) {
+			throw new IllegalStateException("A pizza deve ter pelo menos um ingrediente");
+		}
+
+	}
+	
+	private void validarTamanho() {
+		Integer tamanhoPizza = pizza.getSize();
+		
+		if(tamanhoPizza < 1 || tamanhoPizza > 3) {
+			throw new IllegalStateException("Tamanho da pizza invalido: ");
+		}
+	}
+
+}
