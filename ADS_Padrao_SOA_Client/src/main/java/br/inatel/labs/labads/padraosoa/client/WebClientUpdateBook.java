4878d131ae4b51205869f1483a3aff820f4bb972
Mariana <87673848+MaMBassi@users.noreply.github.com>
2022-12-12 01:17:57 -0300

    SOA-Client

---
+++ ADS_Padrao_SOA_Client/src/main/java/br/inatel/labs/labads/padraosoa/client/WebClientUpdateBook.java
@@ -0,0 +1,27 @@
+package br.inatel.labs.labads.padraosoa.client;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+public class WebClientUpdateBook {
+	public static void main(String[] args) {
+		BookDTO existingBook = new BookDTO();
+		existingBook.setId(3L);
+		existingBook.setTitle("Cozinha Mineira");
+		existingBook.setCategory("Gastronomia");
+		
+		Mono<ResponseEntity<Void>> monoVoid = WebClient.create("http://localhost:8080")
+				.put()
+				.uri("/book")
+				.bodyValue(existingBook)
+				.retrieve()
+				.toBodilessEntity();
+
+		monoVoid.subscribe();
+		ResponseEntity<Void> responseEntity = monoVoid.block();
+		System.out.println( responseEntity.getStatusCode());
+	}
+
+}
